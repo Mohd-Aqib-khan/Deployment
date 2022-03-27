364eb5eacf048d7b0e91935fd103182ad00d0b54
DaffodilAqib <uceraqib786@gmail.com>
2022-03-27 10:16:28 +0530

    change deployment setting

--- rain/settings.py
+++ rain/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["devraindetector.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["deveraindetector.herokuapp.com","localhost"]
 #ALLOWED_HOSTS = ["*"]
 
 # Application definition
