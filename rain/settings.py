e413dd00e8107df839436fdd3df134265e1b81c0
DaffodilAqib <uceraqib786@gmail.com>
2022-03-27 10:41:11 +0530

    commit staticfiless_dirs

--- rain/settings.py
+++ rain/settings.py
@@ -128,9 +128,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 # Default primary key field type
